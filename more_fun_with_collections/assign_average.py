03872e3fdcb00bce995644d4f7ba778a28539e54
G8A4W0416 <wilhelmg1@gmail.com>
2020-03-18 23:28:16 -0500

    Third commit for Module 8 Topic 3 Selection using Dictionary Assignment. Tests for 'A' and 'B' are passing.

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -1,6 +1,7 @@
 def switch_average(grade):
     switcher = {
-        'A': 90
+        'A': 90,
+        'B': 80
     }
     # Second parameter acts as our default
     average = switcher.get(grade)
