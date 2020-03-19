bdcdbda1280869548620b0f61d2f2c7be753ec05
G8A4W0416 <wilhelmg1@gmail.com>
2020-03-19 00:29:45 -0500

    Second commit for Module 8 Topic 4 datetime Assignment with test passing.

--- more_fun_with_collections/half_birthday.py
+++ more_fun_with_collections/half_birthday.py
@@ -1,2 +1,17 @@
+"""
+Program half_birthday.py
+
+Author: Greg Wilhelm
+
+Last date modified: 3/19/2020
+
+The purpose of this program is to get the date of the birthday and return the date six months later.
+
+"""
+
+from datetime import timedelta
+
+
 def half_birthday(date_of_birthday):
-    pass
+    six_months_later = date_of_birthday + timedelta(days=182)
+    return str(six_months_later)
